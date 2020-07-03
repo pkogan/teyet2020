33de164f572ff0fb820da5b3e0536e2d878611a0
pablo <pablo@yenni.fi.uncoma.edu.ar>
2020-07-02 23:47:50 -0300

    cuenta

--- webex.php
+++ webex.php
@@ -2,7 +2,7 @@
 $corte1=new DateTime('2020-07-06 13:30');
 $corte2=new DateTime('2020-07-02 23:59');
 $now=new DateTime();
-//print_r($now->format('Y-m-d H:i:s'));exit();
+print_r($now->format('Y-m-d H:i:s'));exit();
 if($now->getTimestamp()<$corte1->getTimestamp()){
     $url='https://unlpeduar.webex.com/unlpeduar-es/onstage/g.php?MTID=e2c848bbfd603b6bf2e6da97c6dafe595';
 }elseif($now->getTimestamp()<$corte2->getTimestamp()){
