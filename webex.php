de7542c76660f08b7d3d6fde1ce98a05ae2d01e4
pablo <pablo@yenni.fi.uncoma.edu.ar>
2020-07-03 00:02:56 -0300

    cuenta

--- webex.php
+++ webex.php
@@ -1,6 +1,6 @@
 <?php
-$corte1=new DateTime('2020-07-03 00:00');
-$corte2=new DateTime('2020-07-03 00:01');
+$corte1=new DateTime('2020-07-06 13:30');
+$corte2=new DateTime('2020-07-06 23:00');
 $now=new DateTime();
 //print_r($now->format('Y-m-d H:i:s'));exit();
 if($now->getTimestamp()<$corte1->getTimestamp()){
